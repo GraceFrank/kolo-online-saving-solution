437666439829e5f6616868026af2273e1a9123d9
GraceFrank <37621488+GraceFrank@users.noreply.github.com>
2018-11-11 04:28:20 +0100

    Add files via upload

---
+++ js/test.js
@@ -0,0 +1,24 @@
+$('#testStuff').click(function(){
+    //help establish connection to a url that will be specified in a moment
+    var requestCustomer = new XMLHttpRequest();
+    //get json data from the specified url
+    requestCustomer.open('GET', 'http://localhost:3000/customer');
+    //here is what will be done with the data gotten
+    requestCustomer.onload = function(){
+        console.log(requestCustomer.responseText);//this is just to see if our request dey work
+        //now we want to save the data we load to a variable 
+        saveCustomer= requestCustomer.responseText;
+        console.log(saveCustomer[0]); //we only see [ becus our browser is seing the data as test b/c we use .responseText
+
+        /*now we will tell our browser to see it as json file by using the JSON.parse() whichl will tell our browser
+        to filter whatever is inside the parenthesis()as a JSON object*/
+        saveCustomer= JSON.parse(requestCustomer.responseText); 
+        console.log(saveCustomer[0]); //now we can see the object in the console because of the above...
+            };
+
+    requestCustomer.send();
+
+        });
+    //send requst, cuz if you dont send, none of all the instructions given above will work o
+
+  
